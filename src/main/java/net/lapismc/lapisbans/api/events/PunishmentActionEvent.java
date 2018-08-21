0b18ae9dd6a3d52d5113be0bc30355576e106e88
Benjamin <DNAblue2112@gmail.com>
2018-08-21 13:44:55 +0930

    Added JavaDocs to events API and added ActionEvent

---
+++ src/main/java/net/lapismc/lapisbans/api/events/PunishmentActionEvent.java
@@ -0,0 +1,38 @@
+package net.lapismc.lapisbans.api.events;
+
+import net.lapismc.lapisbans.api.punishments.Ban;
+import net.lapismc.lapisbans.api.punishments.Punishment;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * This event is to notify plugins about punishment actions like a player being kicked because they are banned
+ * or a players chat message or command being cancelled because they are muted
+ * Note that you can't cancel this, it is only to notify
+ */
+@SuppressWarnings("WeakerAccess")
+public class PunishmentActionEvent extends Event {
+
+    public static HandlerList handlers = new HandlerList();
+    private Punishment punishment;
+
+    public PunishmentActionEvent(Punishment punishment) {
+        this.punishment = punishment;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    /**
+     * @return Returns the punishment being acted upon, this will be an extension of punishment like {@link Ban}
+     */
+    public Punishment getPunishment() {
+        return punishment;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return null;
+    }
+}
--- src/main/java/net/lapismc/lapisbans/api/events/PunishmentApplicationEvent.java
+++ src/main/java/net/lapismc/lapisbans/api/events/PunishmentApplicationEvent.java
@@ -1,10 +1,15 @@
 package net.lapismc.lapisbans.api.events;
 
+import net.lapismc.lapisbans.api.punishments.Ban;
 import net.lapismc.lapisbans.api.punishments.Punishment;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+/**
+ * This event is fires when a punishment is given to a player
+ * You can get the details of the punishment by checking the type of the punishment and casting it to ban for example
+ */
 @SuppressWarnings("WeakerAccess")
 public class PunishmentApplicationEvent extends Event implements Cancellable {
 
@@ -21,6 +26,9 @@ public class PunishmentApplicationEvent extends Event implements Cancellable {
         return handlers;
     }
 
+    /**
+     * @return Returns the punishment being acted upon, this will be an extension of punishment like {@link Ban}
+     */
     public Punishment getPunishment() {
         return punishment;
     }
@@ -36,11 +44,20 @@ public class PunishmentApplicationEvent extends Event implements Cancellable {
         cancelled = cancel;
     }
 
+    /**
+     * Use this method to cancel the punishment with a reason that will be relayed to the source of the punishment
+     *
+     * @param cancel true if the punishment should be stopped
+     * @param reason The reason that the punishment was cancelled and to be sent to the source of the punishment
+     */
     public void setCancelled(boolean cancel, String reason) {
         cancelled = cancel;
         this.reason = reason;
     }
 
+    /**
+     * @return Returns true if the application of punishment has been cancelled
+     */
     public String getReason() {
         return reason;
     }
