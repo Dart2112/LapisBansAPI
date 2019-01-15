629d0f2126ded70dd5ca7521f0e986b5405c3ae3
dart2112 <DNAblue2112@gmail.com>
2019-01-15 18:08:55 +1030

    Updated PunishmentListInterface to a class as it doesnt depend on LapisBans

--- src/main/java/net/lapismc/lapisbans/api/punishments/core/PunishmentList.java
+++ src/main/java/net/lapismc/lapisbans/api/punishments/core/PunishmentList.java
@@ -16,22 +16,34 @@
 
 package net.lapismc.lapisbans.api.punishments.core;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 /**
  * A class for storing a list of {@link PunishmentInterface}'s. This is required because of the way the API is structured
- * There is no need for the user of this API to use this interface or any implementation of it, it is simply used to make
+ * There is no need for the user of this API to use this class, it is simply used to make
  * converting a list of punishments to a list of {@link PunishmentInterface}'s easier
  */
-public interface PunishmentListInterface {
+public class PunishmentList {
+
+    private List<Object> list;
+
+    /**
+     * Setup the list
+     */
+    public PunishmentList() {
+        list = new ArrayList<>();
+    }
 
     /**
      * Get the number of items currently in the list
      *
      * @return Returns the number of Punishments stored
      */
-    Integer size();
+    public Integer size() {
+        return list.size();
+    }
 
     /**
      * Get the punishment at any index
@@ -39,34 +51,48 @@ public interface PunishmentListInterface {
      * @param index The index you wish to fetch
      * @return Returns the {@link PunishmentInterface} at the index provided
      */
-    PunishmentInterface get(Integer index);
+    public PunishmentInterface get(Integer index) {
+        return (PunishmentInterface) list.get(index);
+    }
 
     /**
      * Add a {@link PunishmentInterface} to the list
      *
      * @param punishment The punishment you wish to add
      */
-    void add(PunishmentInterface punishment);
+    public void add(PunishmentInterface punishment) {
+        list.add(punishment);
+    }
 
     /**
      * Add multiple vales at once
      *
      * @param punishments The collection of {@link PunishmentInterface}'s you wish to add
      */
-    void addAll(Collection<PunishmentInterface> punishments);
+    public void addAll(Collection<PunishmentInterface> punishments) {
+        list.addAll(punishments);
+    }
 
     /**
      * Remove a {@link PunishmentInterface} from the list
      *
      * @param punishment the punishment you wish to remove
      */
-    void remove(PunishmentInterface punishment);
+    public void remove(PunishmentInterface punishment) {
+        list.remove(punishment);
+    }
 
     /**
      * Get the {@link PunishmentInterface}'s currently stored as a {@link List}
      *
      * @return Returns a list of the currently stored punishments
      */
-    List<PunishmentInterface> getAsList();
+    public List<PunishmentInterface> getAsList() {
+        List<PunishmentInterface> interfaceList = new ArrayList<>();
+        for (Object pObject : list) {
+            interfaceList.add((PunishmentInterface) pObject);
+        }
+        return interfaceList;
+    }
 
 }
