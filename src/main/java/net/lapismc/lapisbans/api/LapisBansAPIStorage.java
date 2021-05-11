d0519e90651ee678a32c007ad0665741e47f8999
Benjamin Martin <DNAblue2112@gmail.com>
2021-05-11 12:44:46 +0930

    Update 2.0.8 Changed the way LapisBans adds the API to storage plus minor cleanup

--- src/main/java/net/lapismc/lapisbans/api/LapisBansAPIStorage.java
+++ src/main/java/net/lapismc/lapisbans/api/LapisBansAPIStorage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Benjamin Martin
+ * Copyright 2021 Benjamin Martin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ public class LapisBansAPIStorage {
 
     private static LapisBansAPI api;
 
-    public LapisBansAPIStorage(LapisBansAPI api) {
+    public static void setAPI(LapisBansAPI api) {
         LapisBansAPIStorage.api = api;
     }
 
