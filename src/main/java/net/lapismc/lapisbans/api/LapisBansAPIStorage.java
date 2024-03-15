3c4868f3473c8ac542afe1970432dff69358407c
Dart2112 <dart2112@lapismc.net>
2024-03-15 20:06:20 +1030

    Update 2.1.4 Updating to JDK 17 Setup to release to personal Maven repo

--- src/main/java/net/lapismc/lapisbans/api/LapisBansAPIStorage.java
+++ src/main/java/net/lapismc/lapisbans/api/LapisBansAPIStorage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Benjamin Martin
+ * Copyright 2024 Benjamin Martin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,16 +16,28 @@
 
 package net.lapismc.lapisbans.api;
 
+/**
+ * This class stores the API object from the plugin
+ */
 public class LapisBansAPIStorage {
 
     private static LapisBansAPI api;
 
-    public static void setAPI(LapisBansAPI api) {
-        LapisBansAPIStorage.api = api;
-    }
-
+    /**
+     * Get an instance of the API class from the plugin
+     * @return an instance of the LapisBansAPI interface
+     */
     public static LapisBansAPI getAPI() {
         return api;
+    }
+
+    /**
+     * Do not use, this is used by LapisBans to store an instance of the API here for you to get
+     *
+     * @param api The API instance to be stored
+     */
+    public static void setAPI(LapisBansAPI api) {
+        LapisBansAPIStorage.api = api;
     }
 
 }
